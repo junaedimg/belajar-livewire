38f9f7520fc0304fc5c23995d4fd7e58ade5fd8f
edi <jmg290601@gmail.com>
2025-10-28 23:46:03 +0700

    validasi create user

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,24 +4,51 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Validate;
 
 class Users extends Component
 {
+
+    #[Validate('required|min:3')]
     public $name = '';
+
+    #[Validate('required|email:dns|unique:users')]
     public $email = '';
+
+    #[Validate('required|min:3')]
     public $password = '';
 
     public function createNewUser()
     {
+
+        // ! CARA Liverwire
+        // $validated = $this->validate([
+        //     'name' => 'required|min:3',
+        //     'email' => 'required|email:dns|unique:users',
+        //     'password' => 'required|min:3'
+        // ]);
+
+        // User::Create([
+        //     'name' => $validated['name'],
+        //     'email' => $validated['email'],
+        //     'password' => Hash::make($validated['password'])
+        // ]);
+
+        $this->validate();
+
         User::Create([
             'name' => $this->name,
             'email' => $this->email,
-            'password' => $this->password
+            'password' => Hash::make($this->password)
         ]);
+
+
 
         // $this->reset(['name','email','password']);
         $this->reset();
 
+        session()->flash('success', 'User created.');
     }
 
     public function render()
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -12,22 +12,36 @@
             <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight ">Create New User</h2>
         </div>
 
+        @if (session('success'))
+            <div class="p-4 mb-4 mt-6 text-sm text-green-800 rounded-lg bg-green-50 bg-gray-800 dark:text-green-400"
+                role="alert">
+                {{ session('success') }}
+            </div>
+        @endif
+
         <div class="mt-10">
             <form wire:submit.prevent="createNewUser" method="POST" class="space-y-6">
                 <div>
                     <label for="name" class="block text-sm/6 font-medium text-gray-700">Name</label>
                     <div class="mt-2">
-                        <input id="name" wire:model="name" type="text" name="name" required
-                            autocomplete="name"
+                        <input id="name" wire:model="name" type="text" name="name" autocomplete="name"
                             class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full" />
+                        @error('name')
+                            <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span
+                                    class="font-medium">{{ $message }}</p>
+                        @enderror
+
                     </div>
                 </div>
                 <div>
                     <label for="email" class="block text-sm/6 font-medium text-gray-700">Email address</label>
                     <div class="mt-2">
-                        <input id="email" wire:model="email" type="email" name="email" required
-                            autocomplete="email"
+                        <input id="email" wire:model="email" type="email" name="email" autocomplete="email"
                             class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full" />
+                        @error('email')
+                            <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span
+                                    class="font-medium">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
 
@@ -38,18 +52,24 @@
                             <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot
                                 password?</a>
                         </div>
+
+
                     </div>
                     <div class="mt-2">
-                        <input id="password" wire:model="password" type="password" name="password" required
+                        <input id="password" wire:model="password" type="password" name="password"
                             autocomplete="current-password"
                             class="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full" />
+                        @error('password')
+                            <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span
+                                    class="font-medium">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
 
                 <div>
                     <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold  hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign
-                        in</button>
+                        class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold  hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Create
+                        new user</button>
                 </div>
             </form>
 
